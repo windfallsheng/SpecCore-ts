c8be771485fbcf4330ad6bf5bb319f29b6fb46e5
luzhaosheng <windfallsheng@aliyun.com>
2026-07-11 18:54:24 +0800

    构建文件

--- dist/commands/init.d.ts
+++ dist/commands/init.d.ts
@@ -1,6 +1,7 @@
 export interface InitOptions {
     mode?: string;
     force?: boolean;
+    lang?: string;
 }
 export declare function initCommand(options: InitOptions): Promise<void>;
 //# sourceMappingURL=init.d.ts.map
