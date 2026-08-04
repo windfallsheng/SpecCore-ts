58d6ae308f977fb48ba7be724a33a35399f2eef9
luzhaosheng <windfallsheng@aliyun.com>
2026-08-04 18:40:15 +0800

    chore: stop tracking dist/ directory

--- dist/commands/status.d.ts
+++
@@ -1,7 +0,0 @@
-export interface StatusOptions {
-    iteration?: string;
-    assignee?: string;
-    type?: string;
-}
-export declare function statusCommand(options: StatusOptions): Promise<void>;
-//# sourceMappingURL=status.d.ts.map
